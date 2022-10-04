aab1f5e2d2855c75f799e9424fc9b6748ac25648
LuanNg <thithaicm1@gmail.com>
2022-10-04 23:42:02 +0700

    add nextjs

---
+++ redirect/pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>
+        This link got some problems so i changed it to{" "}
+        <a href="https://vercel.com/hoangluan97/clone">Click here</a>
+      </h1>
+      <p>I'm so sorry about this inconvenience</p>
+    </div>
+  );
+}
